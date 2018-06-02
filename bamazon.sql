10f56c1a0a975724be6ab09f5c0941ba0bc4beea
Seirradawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-06-01 21:29:09 -0400

    creation of functions

--- bamazon.sql
+++ bamazon.sql
@@ -1,13 +1,14 @@
 DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
+
 USE bamazon;
 
 CREATE TABLE products (
-    item_id INT(11) AUTO_INCREMENT NOT NULL, --(unique id for each product)
-    product_name VARCHAR(50) NOT NULL, --(Name of product)
+    item_id INT NOT NULL AUTO_INCREMENT, 
+    product_name VARCHAR(50) NOT NULL, 
     department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,--(cost to customer),
-    stock_quantity INT(11) NOT NULL,--(how much of the product is available in stores)
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL,
     PRIMARY KEY(item_id)
 );
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,72 +1,82 @@
 require("dotenv").config();
+
 // Load the NPM Package inquirer
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var console_table = require("console.table");
+// require("console.table");
 
 // object variable containing settings for connecting to MySQL database
 var connection = mysql.createConnection({
-    host: 'localhost',
-    //The port from MySQL
-    port: 3306,
-    //My username
-    user: "root",
-    //My password that's housed in .env file
-    password: process.env.password, 
-    database: 'bamazon'
+  host: "localhost",
+  //The port from MySQL
+  port: 3306,
+  //My username
+  user: "root",
+  //My password that's housed in .env file
+  password: process.env.password,
+  database: "bamazon"
 });
+
 
 // start database connection && connection successful
 connection.connect(function(err) {
-    if (err) throw err; 
-    console.log('Successful connection! ' + connection.threadId); //threadid is a process id from mySQL 
-    //call function to display the products
-    displayProducts();
-}) // closing connection function
+  if (err) throw err;
+  console.log('Successful connection! ' + connection.threadId); //threadid is a process id from mySQL 
+}); // closing connection function
 
 //query the database for all of the products available for sale (ids, name, and prices)
-function displayProducts() {
-    console.log("Hello && Thanks for shopping @ Bamazon!")
-    connection.query("SELECT * FROM products", function(err,res) {
-        if (err) throw err;
-        console.table(res)
-        displayTable(res);
-        makePurchase();
-    });
-}
+var displayProducts = function () {
+  console.log("Hello && Thanks for shopping @ Bamazon!")
+  connection.query("SELECT * FROM products", function(err,res) {
+    if (err) throw err;
+    console.table(res)
+  })
+};
 
-function displayTable(res) {
-    var table = [];
-    for (var i = 0; i < res.length; i++) {
-        //push products to table
-        table.push({
-            "Product ID": res[i].item_id,
-            "Product Name": res[i].product_name,
-            "Department": res[i].department_name,
-            "Price": "$" + res[i].price,
-            "Quantity": + res[i].stock_quantity
-        }); // end of table.push
-    } //end of for loop
-    console.table(table);
-}// end of displayTable ();
-
-//once the items load, prompt users with two messages in begin purchase
-function makePurchase(){
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    message: "What is product ID you would like to purchase?\n\n",
-                    name: "item_id"
-                    //What is use don't enter number
-                },{
-                    type: "input",
-                    message: "How many units of the product you would like to purchases?\n\n"
-                    name: "stock_quantity"
-                } // end of prompt
-        ])
-        .then(function(answer){
-            connection.query("SELECT * FROM 'products' WHERE ?",)
+var makePurchase = function() {
+  // query the database for all products available for purchase
+  connection.query("SELECT * FROM products", function(err,res) {
+    if (err) throw err;
+    //once the items load, prompt users with two messages in begin purchase
+    inquirer
+      .prompt([
+        {
+          name: "item_id",
+          type: "input",
+          options: function () {
+            var optionArray = [];
+            for (var i = 0; i <res.length; i++) {
+              optionArray.push({
+                "Product ID": res[i].item_id,
+                "Product Name": res[i].product_name,
+                "Department": res[i].department_name,
+                "Price": "$" + res[i].price,
+                "Quantity": + res[i].stock_quantity
+              });
+            }
+            console.table(optionArray);
+          },
+          message: "What product ID you would like to purchase?\n\n"
+          //What is use don't enter number
+        },
+        {
+          name: "stock_quantity",
+          type: "input",
+          message: "How many units of the product you would like to purchases?\n\n"
+        } // end of prompt
+    ])
+    .then(function(answer){
+      var productSelected;
+      for (var i = 0; i < res.length; i++){
+        if (res[i].product_name === answer.product) {
+          productSelected = res[i];
         }
-    });
-});
+      }
+
+      if(productSelected.stock_quantity > parseInt(answer.amount)) {
+        connection.query("UPDATE products SET ? WHERE ?",)
+
+      }
+    })
+  })
+}
